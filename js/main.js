3b61c46d4375eb52d49ccfffcc290d3483f37a34
BhashithaArambepola <barambepola@gmail.com>
2023-04-25 15:36:47 +0530

    change nav

--- js/main.js
+++ js/main.js
@@ -27,8 +27,10 @@
         } else {
             if ($(this).scrollTop() > 45) {
                 $('.fixed-top').addClass('bg-white shadow').css('top', -45);
+                $('.fixed-top').removeClass('bg-transparent ').css('top', 0);
             } else {
                 $('.fixed-top').removeClass('bg-white shadow').css('top', 0);
+                $('.fixed-top').addClass('bg-transparent ').css('top', 0);
             }
         }
     });
